af8002f82675835d9e203473d689beade9efb725
giregk <giregk@users.noreply.github.com>
2026-02-19 09:59:44 +0100

    11975 add typing

--- src/templates/_partials/layout/index.tsx
+++ src/templates/_partials/layout/index.tsx
@@ -1,13 +1,20 @@
 import { Container, Tailwind } from '@react-email/components'
 import { Body } from '@react-email/components'
 import { Head, Html } from '@react-email/components'
+import { ReactNode } from 'react'
 import { IntlProvider } from 'react-intl'
+import { Locales } from 'src/templates/index.js'
 
 import trads from '../footer/messages.json' with { type: 'json' }
 import { Footer, Header } from '../index.js'
 import tailwindConfig from './tailwind.config.js'
 
-export const Layout = ({ children, messages, locale }) => {
+type TLayoutProps = {
+  children: ReactNode
+  messages: { [tradKey: string]: string }
+  locale: Locales
+}
+export const Layout = ({ children, messages, locale }: TLayoutProps) => {
   const mergedMessages = {
     ...trads[locale],
     ...messages,
